de89aed2531304971d8491f744fec6a3f4e95af4
Aleksey Zheryakov <az1773443@gmail.com>
2021-03-21 17:45:49 +0700

    list-from-api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import List from './list'
+import React from 'react'
 import api from './api'
+
 function App() {
-  api.getList()
-  const listFilter = List.filter(item => item.userId % 2 !==0)
-    .sort((a, b) => b.userId - a.userId)
-    .map((item) => {
-      const [ first ] = item.title.split(' ');
-      const body5 = item.body.split(' ',5).join();
+  const [list, setList] = React.useState([]);
+  const [error, setError] = React.useState('');
+  React.useEffect(() => {
+    api.getList().then((data) => {
+      const preparedList = data.filter(item => item.userId % 2 !==0)
+      .sort((a, b) => b.userId - a.userId)
+      .map((item) => {
+        const [ first ] = item.title.split(' ');
+        const body5 = item.body.split(' ',5).join();
   
       return { ...item, title: first, body: body5};
+    
+      })
+      setList(preparedList);
     })
-  return (
-    <div>
-      {listFilter.map(item => (
-        <li key={item.id}>
-          <h3>
-            {item.title}
-          </h3>
-          <p>
-            {item.body}
-          </p>
-        </li>
-      ))}
-    </div>
+      .catch(err => setError("Произошла ошибка сервера!!!"));
+  }, []);
+
+    return (
+      <div>
+        {<div>{`${error}`}</div>}
+        {list.map(item => (
+          <li key={item.id}>
+            <h3>
+              {item.title}
+            </h3>
+            <p>
+              {item.body}
+            </p>
+          </li>
+        ))}
+      </div>
   );
 }
 
--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ const api = {
     getList: () => {
         return fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
-            .then(json => console.log(json))
     }
 
 }
